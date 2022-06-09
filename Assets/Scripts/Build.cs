07617b2524c4a724539ebddf0a9407e8ac70402a
2x1010011010 <s.anisimov.it@gmail.com>
2022-06-09 12:03:46 +0300

    124

--- Assets/Scripts/Build.cs
+++ Assets/Scripts/Build.cs
@@ -15,7 +15,7 @@ public static class Build
         {
             scenes = EditorBuildSettings.scenes.Select(s => s.path).ToArray(),
             target = BuildTarget.Android,
-            locationPathName = $"build/{Application.productName}_{Application.version}.apk"
+            locationPathName = $"../build/{Application.productName}_{Application.version}.apk"
         };
         EditorUserBuildSettings.buildAppBundle = false;
         EditorUserBuildSettings.androidCreateSymbolsZip = false;
