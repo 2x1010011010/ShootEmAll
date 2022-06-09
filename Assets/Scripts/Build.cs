32345582dfac69fbc42ad888e7529d8c9d1bc344
2x1010011010 <s.anisimov.it@gmail.com>
2022-06-09 15:48:57 +0300

    Create Build.cs

---
+++ Assets/Scripts/Build.cs
@@ -0,0 +1,29 @@
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public static class Build
+{
+    private const string PASS = "A2D546CA";
+
+    private static string ProjectFolderPath =>
+        Application.dataPath.Substring(0, Application.dataPath.Length - "Assets/".Length);
+
+    private static void APK()
+    {
+        var buildOptions = new BuildPlayerOptions
+        {
+            scenes = EditorBuildSettings.scenes.Select(s => s.path).ToArray(),
+            target = BuildTarget.Android,
+            locationPathName = $".github/artifacts/{Application.productName}_{Application.version}.apk"
+        };
+        EditorUserBuildSettings.buildAppBundle = false;
+        EditorUserBuildSettings.androidCreateSymbolsZip = true;
+        PlayerSettings.Android.useCustomKeystore = true;
+        PlayerSettings.Android.keystoreName = $"{Application.dataPath}/BUILD/tarragon.keystore";
+        PlayerSettings.Android.keystorePass = PASS;
+        PlayerSettings.Android.keyaliasName = "tarragon";
+        PlayerSettings.Android.keyaliasPass = PASS;
+        BuildPipeline.BuildPlayer(buildOptions);
+    }
+}
